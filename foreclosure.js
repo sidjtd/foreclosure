9d14b0307d90843e8622e0ca6c16e65d57b103eb
Sidney <user@SGD-MBP.local>
2016-06-08 22:05:23 -1000

    foreclosure debug clean up

--- foreclosure.js
+++ foreclosure.js
@@ -1,87 +1,84 @@
 // Step 1
 'use strict';
 var steve;  //Happy Variables
-var stevesLoans;
+var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
 
-//START CODE#######################################################
+function loan() {
 
-function loan(){ // THIS IS A HAPPY FUNCTION
-
-  var account = {         //THIS IS A VARIABLE THAT HAS AN OBJECT ASSOCIATED WITH IT
-    borrowed: 550000,
-    balance: 286000,
-    monthlyPayment: 1700,
-    defaulted: 0,
-    defaultsToForeclose: 5,
-    foreclosed: false               
+  var account = {
+    borrowed : 550000,
+    balance : 286000,
+    monthlyPayment : 1700,
+    defaulted : 0,
+    defaultsToForeclose : 5,
+    foreclosed : false
   };
 
   //VAR OBJECT-----------------------------------
+  // Step 2
 
-// Step 2
-  function missPayment() {          //This is a function
-    account.defaulted = account.defaulted + 1;    
+  function missPayment() {
+    account.defaulted = account.defaulted + 1;
 
-                         //Add 1 to default on account object
-    
-      /* 2.2 */
-    if(account.defaulted >= account.defaultsToForeclose) { 
-        account.foreclosed = true;
-    }    
-  } // function missPayment }}}}}}
+    //Add 1 to default on account object
+    /* 2.2 */
+    if (account.defaulted >= account.defaultsToForeclose) {
+      account.foreclosed = true;
+    }
+  }
 
   // Step 3
   return {
-    getBalance: function(){
-        return account.balance;
-        //3.1.1
-    },// 3.2
-    recievePayment: function(amount){
-        if (amount < account.monthlyPayment) {
-            missPayment();
-        }
-            account.balance = account.balance - amount;
+    getBalance : function() {
+      return account.balance;
+
+      //3.1.1
     },
-    getMonthlyPayment: function(){
-        return account.monthlyPayment;
+    recievePayment : function(amount) {
+      if (amount < account.monthlyPayment) {
+        missPayment();
+      }
+      account.balance = account.balance - amount;
     },
-    isForeclosed: function(){
-        return account.foreclosed;
+    getMonthlyPayment : function() {
+      return account.monthlyPayment;
+    },
+    isForeclosed : function() {
+      return account.foreclosed;
     }
   };// return bracket }}}}}}}
-} 
-function borrower(loan){
+}
+function borrower(loan) {
   var account = {
     monthlyIncome : 1350,
-    funds: 2000,
-    loan: loan.getMonthlyPayment()
+    funds : 2000,
+    loan : loan.getMonthlyPayment()
   };
   return {
-    getFunds: function(){
-        return account.funds;
+    getFunds : function() {
+      return account.funds;
     },
-        makePayment: function(){
-            if(account.funds > account.loan){
-                account.funds = account.funds - account.loan;
-                loan.recievePayment(account.loan);
-            }else{
-                loan.recievePayment(account.funds);
-                account.funds = 0;
-            }
-        },
-        payDay: function(){
-            account.funds = account.funds + account.monthlyIncome;                    
-        }
+    makePayment : function() {
+      if (account.funds > account.loan) {
+        account.funds = account.funds - account.loan;
+        loan.recievePayment(account.loan);
+      }else {
+        loan.recievePayment(account.funds);
+        account.funds = 0;
+      }
+    },
+    payDay : function() {
+      account.funds = account.funds + account.monthlyIncome;
+    }
   };
-}//THIS IS THE BRACKET THAT CLOSES THE ENTIRE LOAN FUNCTION }}}}}}
+}
 
+stevesLoan = loan();
+steve = borrower(stevesLoan);
 
-stevesLoans = loan();
-steve = borrower(stevesLoans);
-
-while(stevesLoans.isForeclosed()===false){
+while (stevesLoan.isForeclosed() === false) {
   steve.payDay();
   steve.makePayment();
   month++;
